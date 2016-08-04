5fd54fe70a6d9e24696ad293a83c0235aab768ae
Jack Hughes <jackhughes1011@gmail.com>
2016-08-04 11:57:23 +0100

    Add files via upload

---
+++ WelcomePage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Diary</title>
+	<link rel="stylesheet" type="text/css" href="diaryStyles.css">
+</head>
+<body>
+
+	<div id="header">
+		Web Diary
+	</div>
+
+	<div class="centered">
+		<br><br>
+		<form id="one" method="GET" action="SignIn.php">
+			<input name="submit" class="b" type="submit" value="Sign In"> 
+		</form>
+		<br><br><br>
+		<form id="two" method="GET" action="SignUp.php">
+			<input name="submit" class="b" type="submit" value="Sign Up">
+		</form>
+	</div>
+
+</body>
+</html>
